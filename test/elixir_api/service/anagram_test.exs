2e355178faaf46fb2d0cf72d15b44ed31d850739
Wilson Martins <martins.wilson17@gmail.com>
2025-01-30 16:36:17 -0500

    Update tests

--- test/elixir_api/service/anagram_test.exs
+++ test/elixir_api/service/anagram_test.exs
@@ -1,3 +1,29 @@
 defmodule ElixirApi.AnagramTest do
   use ExUnit.Case
+  alias ElixirApi.Services.Anagram
+
+  describe "anagrams_in" do
+    test "valid words" do
+      assert Anagram.anagrams_in("castle") == %{
+               3 => ["act", "sea", "set"],
+               4 => [
+                 "case",
+                 "cast",
+                 "east",
+                 "lace",
+                 "last",
+                 "late",
+                 "sale",
+                 "salt",
+                 "seal",
+                 "seat",
+                 "sect"
+               ],
+               5 => ["least", "scale", "slate", "stale", "steal"],
+               6 => ["castle"]
+             }
+
+      assert Anagram.anagrams_in("tests") == %{3 => ["set"], 4 => ["test"], 5 => ["tests"]}
+    end
+  end
 end
--- test/elixir_api/service/dictionary_test.exs
+++ test/elixir_api/service/dictionary_test.exs
@@ -26,41 +26,17 @@ defmodule ElixirApi.DictionaryTest do
     test "with valid words" do
       assert Dictionary.lookup_by_signature("drive")
              |> Enum.sort() ==
-               ["deriv", "diver", "drive", "rived", "verdi"]
-               |> Enum.sort()
-
-      assert Dictionary.lookup_by_signature("walking")
-             |> Enum.sort() ==
-               ["walking"]
+               ["drive"]
                |> Enum.sort()
 
       assert Dictionary.lookup_by_signature("stare")
              |> Enum.sort() ==
-               [
-                 "arest",
-                 "aster",
-                 "astre",
-                 "rates",
-                 "reast",
-                 "resat",
-                 "serta",
-                 "stare",
-                 "strae",
-                 "tares",
-                 "tarse",
-                 "tears",
-                 "teras",
-                 "treas"
-               ]
-               |> Enum.sort()
-
-      assert Dictionary.lookup_by_signature("tenet")
-             |> Enum.sort() ==
-               ["entte", "tenet"]
+               ["rates", "stare", "tears"]
                |> Enum.sort()
     end
 
     test "with invalid word" do
+      assert Dictionary.lookup_by_signature("tenet") == []
       assert Dictionary.lookup_by_signature("ddd") == []
       assert Dictionary.lookup_by_signature("") == []
       assert Dictionary.lookup_by_signature(nil) == []
